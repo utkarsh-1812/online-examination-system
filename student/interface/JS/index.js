2eb90893366fa0af0f5cf73ece09e004e04dbe9d
utkarsh didwania <78554206+utkarsh-1812@users.noreply.github.com>
2023-08-30 16:24:10 +0530

    Delete student directory

--- student/interface/JS/index.js
+++
@@ -1,7 +0,0 @@
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
-
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
-}
